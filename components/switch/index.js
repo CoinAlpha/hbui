7b91c84a33df4429e072079656390fd24b3cd0b3
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-05-02 22:58:18 +0100

    Add switch component

---
+++ components/switch/index.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+import tw, { css } from 'twin.macro'
+
+const stylesA = {}
+stylesA[true] = tw`bg-green relative inline-flex h-6 w-11 items-center rounded-full`
+stylesA[false] = tw`bg-quaternary relative inline-flex h-6 w-11 items-center rounded-full`
+
+const stylesB = {}
+stylesB[true] = tw`translate-x-6 inline-block h-4 w-4 transform rounded-full bg-primary`
+stylesB[false] = tw`translate-x-1 inline-block h-4 w-4 transform rounded-full bg-primary`
+
+function MyToggle({active=false}) {
+  const [enabled, setEnabled] = useState(active)
+  return (
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      tw='transition'
+      css={stylesA[enabled]}
+    >
+      <span
+        css={stylesB[enabled]}
+      />
+    </Switch>
+  )
+}
+
+export default MyToggle
